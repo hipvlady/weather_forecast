4906ac6204150bc5d0ad401507e40f5bce862775
Vlad Parakhin <hipvlady@users.noreply.github.com>
2023-07-19 20:21:28 +0200

    code update with error handling and displaying the img for the sky conditions

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import streamlit as st
 import plotly.express as px
-from services import get_data
-
+from services import get_weather_data
 
 st.title("Weather forecast for the next days")
+
 place = st.text_input("Place: ")
-days = st.slider("Forecast days", min_value=1, max_value=5,
-                 help="Select the number of days to forecast")
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+days = st.slider("Forecast days", min_value=1, max_value=5, help="Select the number of days to forecast")
+data_type = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
+st.subheader(f"{data_type} for the next {days} days in {place}")
 
-d,t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (Celsius)"})
-st.plotly_chart(figure)
+if place:
+    weather_data = get_weather_data(place, days)
 
+    if weather_data is None:
+        st.error("City not found, please check the spelling")
+    else:
+        dates = [data["dt_txt"] for data in weather_data]
 
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
+        if data_type == "Temperature":
+            temperatures = [data["main"]["temp"] / 10 for data in weather_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (Celsius)"})
+            st.plotly_chart(figure)
 
+        elif data_type == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [data["weather"][0]["main"] for data in weather_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
--- services.py
+++ services.py
@@ -1,27 +1,38 @@
 import os
 import requests
+import streamlit as st
 
 
 def get_api_key():
     api_key = os.environ.get('W_API_KEY')
+    if not api_key:
+        st.error("Error: API key is not set.")
+        return None
     return api_key
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_weather_data(place, forecast_days=None):
     api_key = get_api_key()
+    if api_key is None:
+        return None
+
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
+
+    if response.status_code != 200:
+        st.error(f"Error: The API request failed with status code {response.status_code}")
+        return None
+
     data = response.json()
-    filtered_data = data["list"]
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-        if kind == "Sky":
-            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    try:
+        filtered_data = data["list"][:8 * forecast_days] if forecast_days else data["list"]
+    except KeyError:
+        st.error("Error: 'list' key not found in the data.")
+        return None
+
     return filtered_data
 
 
 if __name__ == '__main__':
     print(f"W_API_KEY is {get_api_key()}")
-    print(get_data(place="Kyiv", forecast_days=3, kind="Temperature"))
+    print(get_weather_data(place="Kyiv", forecast_days=3))
